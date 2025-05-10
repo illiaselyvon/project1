479c967248f2e8358e9ed8b785fd5c5f49e9df05
illiaselyvon <i.selevon2017@gmail.com>
2025-05-10 23:58:05 +0200

    Added header and footer

---
+++ index.php
@@ -0,0 +1,252 @@
+
+<?php
+ include('header.php'); 
+?>
+    <div id="services" class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-item">
+                        <span class="service-icon first"></span>
+                        <h3>Responsive Layout</h3>
+                        <p>Sprint is free CSS template based on HTML5 and Bootstrap. You can feel free to download, modify and use this for your website.</p>
+                    </div> <!-- /.service-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-item">
+                        <span class="service-icon second"></span>
+                        <h3>Mobile Friendly</h3>
+                        <p>You may want to visit <a rel="nofollow" href="https://www.toocss.com">Too CSS blog</a> to see latest collections of free CSS templates for your websites. Etiam porttitor nibh et felis molestie fermentum.</p>
+                    </div> <!-- /.service-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-item">
+                        <span class="service-icon third"></span>
+                        <h3>Highest Standard</h3>
+                        <p>Phasellus mauris risus, sodales vitae finibus at, auctor et augue. Fusce est neque, ornare sit amet fringilla eget, tincidunt vel arcu.</p>
+                    </div> <!-- /.service-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-item">
+                        <span class="service-icon fourth"></span>
+                        <h3>Quick Support</h3>
+                        <p>Ut congue interdum pharetra facilisi. Aenean consectetur pellentesque mauris nec ornare. Nam tortor justo, rutrum ut condimentum.</p>
+                    </div> <!-- /.service-item -->
+                </div> <!-- /.col-md-3 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /#services -->
+
+    <div id="product-promotion" class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="section-title">New Arrivals</h1>
+                </div> <!-- /.col-md-12 -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-2 col-sm-3">
+                    <div class="item-small">
+                        <img src="images/promotion/promotion1.jpg" alt="Product 1">
+                        <h4>Nullam Non Ultrices Arcu</h4>
+                    </div> <!-- /.item-small -->
+                </div> <!-- /.col-md-2 -->
+                <div class="col-md-8 col-sm-6">
+                    <div class="item-large">
+                        <img src="images/promotion/promotion2.jpg" alt="Product 2">
+                        <div class="item-large-content">
+                            <div class="item-header">
+                                <h2 class="pull-left">Fusce facilisis semper magna</h2>
+                                <span class="pull-right">Rate: <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                <i class="fa fa-star-half-o"></i></span>
+                                <div class="clearfix"></div>
+                            </div> <!-- /.item-header -->
+                            <p>Maecenas erat lacus, ultrices vel orci ac, eleifend pulvinar est. Proin mollis purus tincidunt enim aliquam posuere et eget magna. Proin sed posuere lorem. Nulla eget neque at eros ultricies venenatis eget sit amet risus. Donec ornare consequat efficitur. <br><br>In hac habitasse platea dictumst. Cras eget scelerisque justo, sit amet aliquam ex. Aliquam iaculis lectus accumsan, egestas lorem ac, malesuada purus.</p>
+                        </div> <!-- /.item-large-content -->
+                    </div> <!-- /.item-large -->
+                </div> <!-- /.col-md-8 -->
+                <div class="col-md-2 col-sm-3">
+                    <div class="item-small">
+                        <img src="images/promotion/promotion3.jpg" alt="Product 3">
+                        <h4>Morbi Sit Amet Lorem</h4>
+                    </div> <!-- /.item-small -->
+                </div> <!-- /.col-md-2 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /#product-promotion -->
+
+    <div id="products" class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="section-title">Products</h1>
+                </div> <!-- /.col-md-12 -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_1.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product1.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Nunc Consectetur</h3>
+                        <span>Price: <em class="text-muted">$250.00</em> - <em class="price">$170.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_3.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product2.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Donec Vitae Massa</h3>
+                        <span>Price: <em class="text-muted">$360.00</em> - <em class="price">$280.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_2.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product3.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Fusce Dignissim Neque</h3>
+                        <span>Price: <em class="text-muted">$560.00</em> - <em class="price">$370.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_1.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product4.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Mauris Adipiscing Enim</h3>
+                        <span>Price: <em class="text-muted">$450.00</em> - <em class="price">$340.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_3.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product5.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Quisque Tincidunt</h3>
+                        <span>Price: <em class="text-muted">$470.00</em> - <em class="price">$360.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_1.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product6.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Aliquam Malesuada</h3>
+                        <span>Price: <em class="text-muted">$425.00</em> - <em class="price">$320.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_2.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product7.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Sed Eleifend Tortor</h3>
+                        <span>Price: <em class="text-muted">$665.00</em> - <em class="price">$450.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="item-thumb">
+                            <span class="note"><img src="images/small_logo_3.png" alt=""></span>
+                            <div class="overlay">
+                                <div class="overlay-inner">
+                                    <a href="#nogo" class="view-detail">Add to Cart</a>
+                                </div>
+                            </div> <!-- /.overlay -->
+                            <img src="images/products/product8.jpg" alt="">
+                        </div> <!-- /.item-thumb -->
+                        <h3>Integer tincidunt</h3>
+                        <span>Price: <em class="text-muted">$840.00</em> - <em class="price">$680.00</em></span>
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /#products -->
+
+    <div id="contact" class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="section-title">Contact</h1>
+                </div> <!-- /.col-md-12 -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8 text-center bigger-text">
+                    <p>Quisque egestas, dui accumsan bibendum semper, eros metus iaculis orci, in fermentum sapien magna quis turpis. Vestibulum ante ipsum primis.</p>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <div id="map">
+                    </div>
+                </div> <!-- /.col-md-6 -->
+                <div class="col-md-6 col-sm-6">
+                    
+                    <div class="row contact-form">
+                    
+                        <fieldset class="col-md-6 col-sm-6">
+                            <input id="name" type="text" name="name" placeholder="Name">
+                        </fieldset>
+                        <fieldset class="col-md-6 col-sm-6">
+                            <input type="email" name="email" id="email" placeholder="Email">
+                        </fieldset>
+                        <fieldset class="col-md-12">
+                            <input type="text" name="subject" id="subject" placeholder="Subject">
+                        </fieldset>
+                        <fieldset class="col-md-12">
+                            <textarea name="comments" id="comments" placeholder="Message"></textarea>
+                        </fieldset>
+                        <fieldset class="col-md-12">
+                            <input type="submit" name="send" value="Send Message" id="submit" class="button">
+                        </fieldset>
+                     
+                    </div> <!-- /.contact-form -->
+                    
+                </div> <!-- /.col-md-6 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /#products -->
+
+    <?php include('footer.php'); ?>
---
+++ partials/header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>Sprint HTML5 Template</title>
+    <meta name="description" content="">
+<!--
+
+Sprint Template
+
+https://templatemo.com/tm-401-sprint
+
+-->
+    <meta name="viewport" content="width=device-width">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/templatemo_misc.css">
+    <link rel="stylesheet" href="css/templatemo_style.css">
+
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+</head>
+<body>
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+    <div id="front">
+        <div class="site-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-6">
+                        <div id="templatemo_logo">
+                            <h1><a href="#">Sprint</a></h1>
+                        </div> <!-- /.logo -->
+                    </div> <!-- /.col-md-4 -->
+                    <div class="col-md-8 col-sm-6 col-xs-6">
+                        <a href="#" class="toggle-menu"><i class="fa fa-bars"></i></a>
+                        <div class="main-menu">
+                            <ul>
+                                <li><a href="#front">Home</a></li>
+                                <li><a href="#services">Services</a></li>
+                                <li><a href="#products">Products</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div> <!-- /.main-menu -->
+                    </div> <!-- /.col-md-8 -->
+                </div> <!-- /.row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="responsive">
+                            <div class="main-menu">
+                                <ul>
+                                    <li><a href="#front">Home</a></li>
+                                    <li><a href="#services">Services</a></li>
+                                    <li><a href="#products">Products</a></li>
+                                    <li><a href="#contact">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.container -->
+        </div> <!-- /.site-header -->
+    </div> <!-- /#front -->
+
+    <div class="site-slider">
+        <ul class="bxslider">
+            <li>
+                <img src="images/slider/slide1.jpg" alt="slider image 1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <div class="slider-caption">
+                                <h2>Your HTML5 Website</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="images/slider/slide2.jpg" alt="slider image 2">
+                <div class="container caption-wrapper">
+                    <div class="slider-caption">
+                        <h2>Free Bootstrap Template</h2>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="images/slider/slide3.jpg" alt="slider image 3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <div class="slider-caption">
+                                <h2>Mobile Ready Design</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="images/slider/slide4.jpg" alt="slider image 4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <div class="slider-caption">
+                                <h2>Responsive Bootstrap</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="images/slider/slide5.jpg" alt="slider image 5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <div class="slider-caption">
+                                <h2>Get it for free!</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul> <!-- /.bxslider -->
+        <div class="bx-thumbnail-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div id="bx-pager">
+                            <a data-slide-index="0" href=""><img src="images/slider/thumb1.jpg" alt="image 1" /></a>
+                            <a data-slide-index="1" href=""><img src="images/slider/thumb2.jpg" alt="image 2" /></a>
+                            <a data-slide-index="2" href=""><img src="images/slider/thumb3.jpg" alt="image 3" /></a>
+                            <a data-slide-index="3" href=""><img src="images/slider/thumb4.jpg" alt="image 4" /></a>
+                            <a data-slide-index="4" href=""><img src="images/slider/thumb5.jpg" alt="image 5" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /.site-slider -->
